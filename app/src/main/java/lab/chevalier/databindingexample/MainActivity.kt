823b1a2900e49cabbc6e27fb86920b9c127eacce
aderesie <aderesie.m.s@gmail.com>
2020-05-13 20:28:09 +0700

    set increase and decrease func

--- app/src/main/java/lab/chevalier/databindingexample/MainActivity.kt
+++ app/src/main/java/lab/chevalier/databindingexample/MainActivity.kt
@@ -2,11 +2,29 @@ package lab.chevalier.databindingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import lab.chevalier.databindingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private var num = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnIncrease.setOnClickListener { increase() }
+        binding.btnDecrease.setOnClickListener { decrease() }
+    }
+
+    private fun increase(){
+        num++
+        binding.tvScore.text = num.toString()
+    }
+
+    private fun decrease(){
+        num--
+        binding.tvScore.text = num.toString()
     }
 }
